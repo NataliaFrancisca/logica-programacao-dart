edd898fe5577349878ddaf0f81bea12377578952
Natalia Francisca <nathifrancisca@gmail.com>
2023-03-08 20:20:43 -0300

    feat: finish the classes about operadores logicos

---
+++ operadores logicos/and.dart
@@ -0,0 +1,12 @@
+// &&
+void main(){
+  final a = 120;
+  final b = 100;
+
+  if((a >= 100) && (b >= 100)){
+    print("são maiores que 100");
+  }else{
+    print("não são todos maior que 100");
+  }
+
+}
---
+++ operadores logicos/or.dart
@@ -0,0 +1,14 @@
+void main(){
+  final a = 80;
+  final b = 20;
+
+  if((a >= 100) || (b >= 100)){
+    print("alguém é maior que 100");
+  }else{
+    print("nenhum é maior que 100");
+  }
+
+  // é verdadeiro se uma das condições for verdadeira
+
+
+}
